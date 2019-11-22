341c48cbcbbf29c779fa735685ba287e54e54920
Ravi Garbuja Pun <cbpmgr@gmail.com>
2019-11-23 00:20:54 +0545

    Write Failing test for Password Field

--- app/src/androidTest/java/com/ravigarbuja/devfest2019/MainActivityTest.kt
+++ app/src/androidTest/java/com/ravigarbuja/devfest2019/MainActivityTest.kt
@@ -2,6 +2,7 @@ package com.ravigarbuja.devfest2019
 
 import androidx.test.espresso.Espresso.onView
 import androidx.test.espresso.action.ViewActions.replaceText
+import androidx.test.espresso.action.ViewActions.typeText
 import androidx.test.espresso.assertion.ViewAssertions.matches
 import androidx.test.espresso.matcher.ViewMatchers.*
 import androidx.test.ext.junit.runners.AndroidJUnit4
@@ -32,7 +33,7 @@ class MainActivityTest{
     }
 
     @Test
-    fun emptyEmail_startWritingEmail_validateEmailField(){
+    fun emptyEmail_startWritingEmail_invalidateEmailField(){
         onView(withId(R.id.et_email))
             .perform(replaceText(invalidEmail1))
             .check(matches(hasErrorText(activityRule.activity.getString(R.string.invalid_email))))
@@ -53,6 +54,13 @@ class MainActivityTest{
         onView(withId(R.id.et_email))
             .perform(replaceText(validEmail))
             .check(matches(not(hasErrorText(activityRule.activity.getString(R.string.invalid_email)))))
+    }
+
+    @Test
+    fun noInput_typeInvalidPassword_invalidatePasswordField(){
+        onView(withId(R.id.et_password))
+            .perform(typeText("short"))
+            .check(matches(hasErrorText("Too Short")))
     }
 
 }
